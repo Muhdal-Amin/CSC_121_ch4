56f697ebfc4f344eb20b9bbd3d9342ca699c0c9a
Muhdal-Amin <adalemo2000@gmail.com>
2023-03-13 17:58:05 +0000

    Initial commit

--- bar_chart/bar_chart_printing.c
+++ bar_chart/bar_chart_printing.c
@@ -1,92 +1,35 @@
-// Paul Dietel C-how to program: Chapter4, Exercise 4.18
-// Program control in C
-
 #include <stdio.h>
 
-// Function main - Code entry point
 int main(void) {
 
-	// Initialization phase
-	// Variables declaration
-	int a,b,c,d,e,f;
+	int number[5];
+	int input;
 
-	// Processing Phase 1
-	printf("%s","Enter a number between 1 and 30: ");
-	scanf("%d", &b);
+	for (int i = 0; i < 5; i++) {
 
-	if (b <= 30) {
+		printf("Enter number #%d: ", i+1);
+		scanf("%d", &input);
 
-		for(a = 1; a <= b; a++) {
-			printf("*\t");
+		while(input < 1 || input > 30){
+		puts("Input must be within 1-30!");
+		printf("Enter number #%d: ", i+1);
+		scanf("%d", &input);
 		}
+		number[i] = input;
 	}
+		puts("");
+		puts("BAR CHART");
 
-	else {
-		printf("Value out of range!\n");
-	}
+		for (int i = 0; i < 5; i++) {
 
-	// Processing Phase 2
-	printf("%s","\n\nEnter a number between 1 and 30: ");
-	scanf("%d", &c);
+			printf("%.2d:", number[i]);
+			for (int j = 0; j < number[i]; j++) {
 
-	if (c <= 30) {
+				printf("*");
+			}
 
-		for(a = 1; a <= c; a++) {
-			printf("*\t");
-
+			puts("");
 		}
-	}
 
-	else {
-		printf("Value out of range!\n");
-	}
-
-	// Processing Phase 3
-	printf("%s","\n\nEnter a number between 1 and 30: ");
-	scanf("%d", &d);
-
-	if (d <= 30) {
-
-		for(a = 1; a <= d; a++) {
-			printf("*\t");
-
-		}
-	}
-
-	else {
-		printf("Value out of range!\n");
-	}
-
-	// Processing Phase 4
-	printf("%s","\n\nEnter a number between 1 and 30: ");
-	scanf("%d", &e);
-
-	if (e <= 30) {
-
-		for(a = 1; a <= e; a++) {
-			printf("*\t");
-
-		}
-	}
-
-	else {
-		printf("Value out of range!\n");
-	}
-
-	// Processing Phase 5
-	printf("%s","\n\nEnter a number between 1 and 30: ");
-	scanf("%d", &f);
-
-	if (f <= 30) {
-
-		for(a = 1; a <= f; a++) {
-			printf("*\t");
-
-		}
-	}
-
-	else {
-		printf("Value out of range!\n");
-	}
-
-} // End function main
+	return(0);
+}
--- calculating_sales/calculating_sales.c
+++ calculating_sales/calculating_sales.c
@@ -9,63 +9,77 @@ int main(void){
 	// Initialization phase
 	// Variable declaration
 	int productNumber;
-	int quantity;
-	float total;
-	total = 0;
+	int quantity, day;
+	float retailValue = 0;
+	float retailValuePerDay = 0;
+	float totalRetailValue = 0;
 
 	// Processing phase
-	printf("Enter product number 1-5 or -1 to end.\n");
-	scanf("%d", &productNumber); // Prompts user to input product number which leads program into a sentinel controlled while loop
-	while (productNumber != -1) {
 
-		switch (productNumber){
+	for (day = 1; day <= 7; day++) {
+		printf("DAY %d: \n", day);
 
-		case 1: printf("%s", "Enter quantity of product 1.\n");
-			scanf("%d", &quantity);
-			total = (float) total + ( 2.98 * quantity );
-			printf("Total retail value is $%.2f.\n\n", total);
-			break;
+		printf("Enter product number 1-5 or -1 to end:\n");
+		scanf("%d", &productNumber); // Prompts user to input product number which leads program into a sentinel controlled while loop
 
-		case 2: printf("%s", "Enter quantity of product 2.\n");
-			scanf("%d", &quantity);
-			total = (float) total + ( 4.50 * quantity );
-			printf("Total retail value is $%.2f.\n\n", total);
-			break;
+		while (productNumber != -1) {
 
-		case 3: printf("%s", "Enter quantity of product 3.\n");
-			scanf("%d", &quantity);
-			total =(float) total + ( 9.98 * quantity );
-			printf( "Total retail value is $%.2f.\n\n", total);
-			break;
+			switch (productNumber){
 
-		case 4: printf("%s", "Enter quantity of product 4.\n");
-			scanf("%d", &quantity);
-			total =(float) total + ( 4.49 * quantity );
-			printf( "Total retail value is $%.2f.\n\n", total);
-			break;
+			case 1: printf("%s", "Enter quantity of product 1:\n");
+				scanf("%d", &quantity);
+				retailValue = retailValue + ( 2.98 * quantity );
+				printf("retail value is $%.2f.\n\n", retailValue);
+				break;
 
-		case 5: printf("%s","Enter quantity of product 5.\n");
-			scanf("%d", &quantity);
-			total =(float) total + ( 6.87 * quantity );
-			printf( "Total retail value is $%.2f.\n\n", total);
-			break;
+			case 2: printf("%s", "Enter quantity of product 2:\n");
+				scanf("%d", &quantity);
+				retailValue = retailValue + ( 4.50 * quantity );
+				printf("retail value is $%.2f.\n\n", retailValue);
+				break;
 
-		default:
-			printf( "Sorry, invalid product number entered!\n\n");
+			case 3: printf("%s", "Enter quantity of product 3:\n");
+				scanf("%d", &quantity);
+				retailValue = retailValue + ( 9.98 * quantity );
+				printf( "Total retail value is $%.2f.\n\n", retailValue);
+				break;
 
-		} // End switch
+			case 4: printf("%s", "Enter quantity of product 4:\n");
+				scanf("%d", &quantity);
+				retailValue = retailValue + ( 4.49 * quantity );
+				printf( "retail value is $%.2f.\n\n", retailValue);
+				break;
 
-		printf( "Enter product number (1-5) or -1 to exit.\n");
-		scanf("%d", &productNumber);
+			case 5: printf("%s","Enter quantity of product 5:\n");
+				scanf("%d", &quantity);
+				retailValue = retailValue + ( 6.87 * quantity );
+				printf( "retail value is $%.2f.\n\n", retailValue);
+				break;
 
-	} // End while
+			default:
+				printf( "Sorry, invalid product number entered!\n\n");
+				break;
 
-	if (productNumber == -1) { // ends the prompt for asking for products and ends with the total sales of the past week
+			} // End switch
 
-		printf("The total sales for the past week is $%.2f\n", total*7);
 
-	} // End if
+			retailValuePerDay =+ retailValue;
 
-	return (0);
+			printf("Enter product number (1-5) or -1 to exit:\n");
+			scanf("%d", &productNumber);
+
+		} // End while
+
+		if (productNumber == -1) { // ends the prompt for asking for products and ends with the total sales for the day
+
+			printf("The total retail value for day %d is : %.2f\n\n" , day, retailValuePerDay);
+
+		} // End if
+
+	} // End for
+
+	totalRetailValue += retailValuePerDay;
+
+	printf ("\nThe total retail value for the past week is : %.2f\n", totalRetailValue);
 
 } // End main function
